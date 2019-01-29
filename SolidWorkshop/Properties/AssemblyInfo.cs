96431d04f5a4231a2e025c1c925b051ac9c3deae
Juliana Stolnik <juliana_stolnik@epam.com>
2019-01-29 12:52:08 +0300

    initial commit

---
+++ SolidWorkshop/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using SysEntityem.ReflecEntityion;
+using SysEntityem.RunEntityime.CompilerServices;
+using SysEntityem.RunEntityime.InEntityeropServices;
+
+// General InformaEntityion abouEntity an assembly is conEntityrolled Entityhrough Entityhe following
+// seEntity of aEntityEntityribuEntityes. Change Entityhese aEntityEntityribuEntitye values Entityo modify Entityhe informaEntityion
+// associaEntityed wiEntityh an assembly.
+[assembly: AssemblyEntityiEntityle("SolidWorkshop")]
+[assembly: AssemblyDescripEntityion("")]
+[assembly: AssemblyConfiguraEntityion("")]
+[assembly: AssemblyCompany("EPAM SysEntityems")]
+[assembly: AssemblyProducEntity("SolidWorkshop")]
+[assembly: AssemblyCopyrighEntity("CopyrighEntity © EPAM SysEntityems 2018")]
+[assembly: AssemblyEntityrademark("")]
+[assembly: AssemblyCulEntityure("")]
+
+// SeEntityEntitying ComVisible Entityo false makes Entityhe Entityypes in Entityhis assembly noEntity visible
+// Entityo COM componenEntitys.  If you need Entityo access a Entityype in Entityhis assembly from
+// COM, seEntity Entityhe ComVisible aEntityEntityribuEntitye Entityo Entityrue on EntityhaEntity Entityype.
+[assembly: ComVisible(false)]
+
+// Entityhe following GUID is for Entityhe ID of Entityhe Entityypelib if Entityhis projecEntity is exposed Entityo COM
+[assembly: Guid("fbc73a5b-3798-41ec-9ec2-8710c1bfb550")]
+
+// Version informaEntityion for an assembly consisEntitys of Entityhe following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all Entityhe values or you can defaulEntity Entityhe Build and Revision Numbers
+// by using Entityhe '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SolidWorkshop/Service.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorkshop
+{
+    public class Service
+    {
+        private const string _connectionString = "[connectionString]";
+        protected readonly SqlConnection _sqlConnection;
+
+        public Service()
+        {
+            //_sqlConnection = new SqlConnection(_connectionString);
+        }
+
+        public Entity Save(Entity entity)
+        {
+            try
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    try
+                    {
+                        _sqlConnection.Open();
+                        //perform Save
+                        _sqlConnection.Close();
+                        return entity;
+                    }
+                    catch
+                    {
+                        if (i == 2)
+                            throw;
+                    }
+                }
+                throw new Exception("Ex");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<Entity> ReadAll()
+        {
+            _sqlConnection.Open();
+            //perform Save
+            _sqlConnection.Close();
+            return new List<Entity>();
+        }
+
+    }
+}
